83111363bada9d49676c7d9d6f478e37bbbeec24
romanjhyltsou <romanjhyltsou@gmail.com>
2023-02-01 23:43:12 +0300

    feat: setTextColor

--- app.js
+++ app.js
@@ -14,11 +14,20 @@ function generateRandomColor(){
 function setRandomColors(){
     cols.forEach(col =>{
         const text = col.querySelector('h2');
-        const color = generateRandomColor();
+        const button = col.querySelector('button');
+        const color = chroma.random();
 
         text.textContent = color;
         col.style.background = color;
+
+        setTextColor(text, color);
+        setTextColor(button, color);
     });
+}
+
+function setTextColor(text, color){
+   const luminance = chroma(color).luminance();
+   text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
 setRandomColors();
