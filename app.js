0ab9d023f9e4755143fb4d3043ded1d9bcd54117
romanjhyltsou <romanjhyltsou@gmail.com>
2023-02-02 00:31:01 +0300

    feat:color  lock-open

--- app.js
+++ app.js
@@ -33,9 +33,14 @@ function generateRandomColor(){
 
 function setRandomColors(){
     cols.forEach(col =>{
+        const isLocked = col.querySelector('i').classList.contains('fa-lock');
         const text = col.querySelector('h2');
         const button = col.querySelector('button');
         const color = chroma.random();
+
+        if(isLocked){
+            return;
+        }
 
         text.textContent = color;
         col.style.background = color;
