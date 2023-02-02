001b4ff75455462b27cc0642ddb91057dcf2e918
romanjhyltsou <romanjhyltsou@gmail.com>
2023-02-02 22:32:52 +0300

    feat:getColorsFromHash

--- app.js
+++ app.js
@@ -37,22 +37,38 @@ function copyToClickboard(text){
     return navigator.clipboard.writeText(text);
 }
 
-function setRandomColors(){
-    cols.forEach(col =>{
+function setRandomColors(isInitial){
+    const colors = isInitial ? getColorsFromHash() : [];
+
+    cols.forEach((col, index )=>{
         const isLocked = col.querySelector('i').classList.contains('fa-lock');
         const text = col.querySelector('h2');
         const button = col.querySelector('button');
-        const color = chroma.random();
+
 
         if(isLocked){
+            colors.push(text.textContent);
             return;
         }
+
+        const color = isInitial 
+        ? colors[index] 
+         ? colors[index]
+         : chroma.random()
+        : chroma.random();
+
+        if(!isInitial){
+            colors.push(color);
+        }
+        
 
         text.textContent = color;
         col.style.background = color;
 
         setTextColor(text, color);
         setTextColor(button, color);
+
+        updateColorsHash(colors);
     });
 }
 
@@ -61,4 +77,23 @@ function setTextColor(text, color){
    text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
-setRandomColors();
+function updateColorsHash(colors = []){
+    document.location.hash = colors
+    .map((col) =>{
+        return col.toString().substring(1);
+    })
+    .join('-');
+    
+}
+
+function getColorsFromHash(){
+    if(document.location.hash.length > 1){
+      return document.location.hash
+      .substring(1)
+      .split('-')
+      .map(color => '#' + color);
+    }
+    return [];
+}
+
+setRandomColors(true);
